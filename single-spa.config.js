37d8da5bb92c634fbcfecfeab5ce8dc342526cda
Christian Lüdemann <christianlydemann@gmail.com>
2019-10-16 16:36:17 +0200

    chore: init

---
+++ single-spa.config.js
@@ -0,0 +1,33 @@
+import { registerApplication, start } from "single-spa";
+
+function pathPrefix(prefix) {
+  return function(location) {
+    return location.pathname.startsWith(prefix);
+  };
+}
+
+registerApplication(
+  "navBar",
+  () => import("./src/navBar/navBar.app.js").then(module => module.navBar),
+  () => true
+);
+
+registerApplication(
+  // Name of our single-spa application
+  "home",
+  // Our loading function
+  () => import("./src/home/home.app.js"),
+  // Our activity function
+  () =>
+    location.pathname === "" ||
+    location.pathname === "/" ||
+    location.pathname.startsWith("/home")
+);
+
+registerApplication(
+  "angularJS",
+  () => import("./src/angularJS/angularJS.app.js"),
+  pathPrefix("/angularJS")
+);
+
+start();
